a98b9553403015b41e02492d6c53c4ad8e057521
oanhnt <oanhk3b@gmail.com>
2015-06-17 12:07:56 +0200

    Update ControllerCompany.php

--- ControllerCompany.php
+++ ControllerCompany.php
@@ -1,23 +1,28 @@
+<?php mysql_connect("localhost", "root", "") or
+    die("Could not connect: " . mysql_error());
+mysql_select_db("data"); ?>
+
 <?php
-/**
- * Project EXP Laravel
- * User: nguyenhieptn
- * Date: 6/14/2015
- * Time: 5:23 PM
- */
+
 
 class ControllerCompany {
+
+
     /*get lowest price */
     public function getLowestPrice(){
-        $idd = $_REQUEST['ID_D'];
+	
+       $idd = $_REQUEST['ID_D'];
+       $sl= mysql_query("select Koszt from listadania where ID_D='D1' order by Koszt desc");
+		$price = array();
+		$price = mysql_fetch_array($sl);
 
-
-        //1. select all prices from all companies
+        //1. select all prices from all companies: "select price,companyname from companies where idd=$idd order by price  desc"
+		//select Koszt from oferta_dania where ID_D=$idd order by Koszt desc;
+		
 
         //2. select the lowest
-        $price = array();
-        $price['lowestprice'] = 333;
-        $price['company'] = "coca";
+        
+
 
         echo json_encode($price);
     }
