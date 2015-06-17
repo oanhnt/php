a2116238247b3a89c2ad797f79eb37baa328e84d
oanhnt <oanhk3b@gmail.com>
2015-06-17 12:08:48 +0200

    Update ControllerOrder.php

--- ControllerOrder.php
+++ ControllerOrder.php
@@ -1,11 +1,6 @@
 <?php
 require_once "orders/order.php";
-/**
- * Project EXP Laravel
- * User: nguyenhieptn
- * Date: 6/14/2015
- * Time: 3:42 PM
- */
+
 
 class ControllerOrder {
     var $orderCode = "";
@@ -31,7 +26,7 @@ class ControllerOrder {
     {
         $orderNumber = $_GET['order'];
         $order = New Order($orderNumber);
-
+		
         $food = array();
         $food['ID_D']         = $_REQUEST['ID_D'];
         $food['Nazwadania']   = $_REQUEST['Nazwadania'];
@@ -39,14 +34,27 @@ class ControllerOrder {
         $food['Skladniki']    = $_REQUEST['Skladniki'];
         $food['Datawyslania'] = $_REQUEST['Datawyslania'];
         $food['Dataimprezy']  = $_REQUEST['Dataimprezy'];
-
-        if($_REQUEST['ftype']=='food'){
-            $order->addFood($food);
-        }else if($_REQUEST['ftype']=='drink'){
-            $order->addDrink($food);
-        }else{
+		$drink=array();
+       // $food['ID_A']         = $_REQUEST['ID_A'];
+        //$food['NazwaAlkoholu']   = $_REQUEST['NazwaAlkoholu'];
+		//$drink['Typ_A']        = $_REQUEST['Typ_A'];
+       // $food['Ilosc_A']    = $_REQUEST['Ilosc_A'];
+        //$food['Datawyslania_A'] = $_REQUEST['Datawyslania_A'];
+        //$food['Dataimprezy_A']  = $_REQUEST['Dataimprezy_A'];
+	 if($_REQUEST['ftype']=='food'){
+            $order->addFood($food);	
+	 }else if($_REQUEST['ftype']=='drink'){
+            $order->addDrink($drink);
+		
+			
+		
+}else{
             echo "no type";
         }
+		
+        
+       
+        
     }
 
     public function testAction()
