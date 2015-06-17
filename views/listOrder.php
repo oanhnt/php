c8a8413b61e9da9ca0ffe7c5d1f2bf3938a02e52
oanhnt <oanhk3b@gmail.com>
2015-06-17 12:11:09 +0200

    Update listOrder.php

--- views/listOrder.php
+++ views/listOrder.php
@@ -3,33 +3,37 @@
     $xml = simplexml_load_file("orders/".$ordernumber.".xml") or die("Error: Cannot create object");
 ?>
 
+<form action="index.php?action=processorder&order=<?php echo $orderNumber ?>" method="post">>
 <table border="1">
-    <tr>
+    <!--
+	<tr>
         <td colspan="5">
             <h4>Drink</h4>
         </td>
     </tr>
     <tr>
-        <td><h5>ddd</h5></td>
-        <td><h5>ddd</h5></td>
-        <td><h5>ddd</h5></td>
-        <td><h5>ddd</h5></td>
-    </tr>
-    <tr>
-        <td>ID</td>
-        <td>dd</td>
-        <td>df</td>
-        <td>k</td>
-    </tr>
-    <tr>
-        <td>ID</td>
-        <td>dd</td>
-        <td>df</td>
-        <td>k</td>
-    </tr>
+	            
+		
+        <td><h5>ID_A</h5></td>
+        <td><h5>NazwaAlkoholu</h5></td>
+        <td><h5>Typ_A</h5></td>
+        <td><h5>Datawyslania_A</h5></td>
+		<td><h5>Dataimprezy_A</h5></td>
 
+    </tr>
+     <?php foreach($xml->order->foods->food as $drink) :?>
+        <tr>
+            <td><?php echo $drink->ID_A ;?></td>
+            <td><?php echo $drink->NazwaAlkoholu ;?></td>
+            <td><?php echo $drink->Typ_A ;?></td>
+            <td><?php echo $drink->Datawyslania_A ;?></td>
+			<td><?php echo $drink->Dataimprezy_A ;?></td>
+        </tr>
+    <?php endforeach; ?>
+    
+-->
     <tr>
-        <td colspan="5">
+        <td colspan="6">
             <h4>Food</h4>
         </td>
     </tr>
@@ -38,6 +42,8 @@
         <td><h5>Nazwadania </h5></td>
         <td><h5>Ilosc</h5></td>
         <td><h5>Skladniki</h5></td>
+		<td><h5>Datawyslania</h5></td>
+		<td><h5>Dataimprezy</h5></td>
     </tr>
     <?php foreach($xml->order->foods->food as $food) :?>
         <tr>
@@ -45,8 +51,20 @@
             <td><?php echo $food->Nazwadania ;?></td>
             <td><?php echo $food->Ilosc ;?></td>
             <td><?php echo $food->Skladniki ;?></td>
+			<td><?php echo $food->Datawyslania ;?></td>
+			<td><?php echo $food->Dataimprezy ;?></td>
         </tr>
     <?php endforeach; ?>
 
 
 </table>
+<tr>
+<h2></h2>
+</tr>
+<tr>
+	<td>
+	
+    <input type="submit" name="send" value="Send"/>
+	</td>
+</tr>
+</form>
